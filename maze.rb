e4acd37be973e9ec469e33a4f1b86e5566efb9f6
Kevin McMorrow <kmcm07@gmail.com>
2014-03-27 10:17:41 +0000

    Removed get_cell method and refactored find_neighbour.

--- maze.rb
+++ maze.rb
@@ -1,4 +1,6 @@
-# http://mazeworks.com/mazegen/mazetut/index.htm
+# http://mazeworks.com/mazegen/mazetut/index.htm 
+# each cell is represented by 16-bits as shown below:
+#
 # backtrack solution  border    walls
 # 0 0 0 0   0 0 0 0   0 0 0 0   0 0 0 0
 # W S E N   W S E N   W S E N   W S E N
@@ -37,6 +39,7 @@ class Maze
 
   private
 
+  # turns on borders on all edge cells
   def setup_borders
     @maze.each do |row|
       row[0] |= 0b10000000
@@ -46,6 +49,7 @@ class Maze
     @maze[-1].each_index { |cell| @maze[-1][cell] |= 0b1000000 }
   end
 
+  # generate a random maze
   def generate_maze
     cell_stack = []
     total_cells = @maze.length * @maze[0].length
@@ -53,7 +57,7 @@ class Maze
     visited_cells = 1
     
     while visited_cells < total_cells
-      neighbour = find_neighbour current_cell
+      neighbour = find_neighbour current_cell[:x], current_cell[:y]
       unless neighbour.nil?
         knock_down_wall current_cell, neighbour
         cell_stack << current_cell
@@ -65,32 +69,19 @@ class Maze
     end
   end
 
-  def find_neighbour(cell)
+  # given a cell, returns a random neighbour that has all 4 walls intact
+  # or nil if no valid neighbour
+  def find_neighbour(x, y)
     neighbours = []
-    if get_cell(cell) & WEST_BORDER == 0
-      neighbours << { x: cell[:x] - 1, y: cell[:y] }
-    end
-    if get_cell(cell) & EAST_BORDER == 0
-      neighbours << { x: cell[:x] + 1, y: cell[:y] }
-    end
-    if get_cell(cell) & NORTH_BORDER == 0
-      neighbours << { x: cell[:x], y: cell[:y] - 1 }
-    end
-    if get_cell(cell) & SOUTH_BORDER == 0
-      neighbours << { x: cell[:x], y: cell[:y] + 1 }
-    end
-
-    neighbours.select! do |c|
-      get_cell(c) & ALL_WALLS == 0b1111
-    end
-
-    unless neighbours.empty?
-      neighbours.sample
-    else
-      nil
-    end
+    neighbours << { x: x - 1, y: y } if @maze[y][x] & WEST_BORDER == 0
+    neighbours << { x: x + 1, y: y } if @maze[y][x] & EAST_BORDER == 0
+    neighbours << { x: x, y: y - 1 } if @maze[y][x] & NORTH_BORDER == 0
+    neighbours << { x: x, y: y + 1 } if @maze[y][x] & SOUTH_BORDER == 0
+    neighbours.select! { |c| @maze[c[:y]][c[:x]] & ALL_WALLS == 0b1111 }
+    neighbours.sample
   end
 
+  # removes the wall between two given cells
   def knock_down_wall(cell1, cell2)
     if cell1[:y] == cell2[:y] # beside each other
       if cell1[:x] < cell2[:x]
@@ -109,10 +100,6 @@ class Maze
         @maze[cell2[:y]][cell2[:x]] ^= SOUTH_WALL
       end
     end
-  end
-
-  def get_cell(location)
-    @maze[location[:y]][location[:x]]
   end
 end
 
